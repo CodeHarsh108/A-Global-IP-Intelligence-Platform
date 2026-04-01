875af9c4a6d936c63cf88dadb5aa790981a17c7c
Nithin0100 <bnithinbharadwaj2005@gmail.com>
2026-04-01 19:16:09 +0530

    Delete sample_spec.cy.js

--- sample_spec.cy.js
+++
@@ -1,8 +0,0 @@
-// cypress/e2e/sample_spec.cy.js
-
-describe('Landing Page', () => {
-  it('loads and displays the login button', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Login').should('be.visible');
-  });
-});
